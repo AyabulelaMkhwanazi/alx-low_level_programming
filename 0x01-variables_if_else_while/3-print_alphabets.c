cbba2b6c33190d21f036dbe09403612553b414dc
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-17 06:21:57 -0800

    Aplhabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		putchar(c);
 		c++;
-		while(d <= 'Z')
-		{
-			putchar(d);
-			d++;
-		}
+	}
+	while(d <= 'Z')
+	{
+		putchar(d);
+		d++;
 	}
 	putchar('\n');
 	return (0);
