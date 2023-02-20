cd8d12038f0733c1aeff5f9f00f53496a2e7f2b2
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-19 22:35:32 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
 /**
- * main: Program that uses function int
- * and uses putchar twice
+ * main: Print base 10 digits , function int
+ * use putchar twice
+ *
  * Return: Always 0 (Success)
  *
- * Only use int  and putchar
- * can only use putchar twice
- *
- * print base 10 numbers from 0
- * followed by new line
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar((i % 10) + '0');
 		i++;
 	}
 	putchar('\n');
