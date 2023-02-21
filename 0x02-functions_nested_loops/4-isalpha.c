58f9cc2d29fa4947a820e9a30ec9a9ce4ec84d08
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-21 08:03:35 -0800

    ALPHABETIC CHARACTERS

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - This function checks for alphabetic characters
+ * @c: the character to print
+ * Return: 1 (success) , otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 97 && c <= 122);
+}
