4e102076b1e198681df6f96a91c5c6c0779fd5c5
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-21 04:03:06 -0800

    Main.h file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - This program prints putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int c = _putchar;
+
+	putchar('\n');
+	putchar(c);
+}
+
