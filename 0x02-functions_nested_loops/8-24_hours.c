4303f6bd9e2fffa2285c1b4d9eb5a3b64e1ed194
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-21 22:22:35 -0800

    HOURS AND MINUTES

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 
 	int hour = 0;
 
-	while (hour < 24)
+	while (hour <= 23)
 	{
 		minute = 0;
 
-		while (minute < 60)
+		while (minute <= 59)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
