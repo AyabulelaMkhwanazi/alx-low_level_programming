05e232ca40bf79b7c85313fda5cea408ca6bade9
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-23 13:55:56 -0800

    Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,9 +14,7 @@ int main(void)
 
 	while (a <= 100)
 	{
-		if (a == 100)
-			printf("Buzz ");
-		else if (a % 3 == 0 && a % 5 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
 		else if (a % 3 == 0)
 			printf("Fizz ");
