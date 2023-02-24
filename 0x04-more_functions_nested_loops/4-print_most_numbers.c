0c39585bda8b55e22ab3fa31310d049d8eb801cc
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-24 07:06:49 -0800

    Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,12 @@
 
 void print_most_numbers(void)
 {
-	int a = 0;
+	int a;
 
-	while (a <= 9);
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != '2' || a != '4')
-		{
-			_putchar('0' + a);
-		}
+		if (a != 2 && a != 4)
+			_putchar(48 + a);
 	}
 	_putchar('\n');
-	a++;
-
 }
