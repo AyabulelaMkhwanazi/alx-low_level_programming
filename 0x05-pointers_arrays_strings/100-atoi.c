ae4375cbb127c9dcdfb5aa00c7fd3f94dc607fcc
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-02-28 06:47:30 -0800

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,6 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
  * Return: the int converted from the string
  */
 
@@ -34,7 +33,7 @@ int _atoi(char *s)
 				digit = -digit;
 
 			n = n * 10 + digit;
-                        f = 1;
+			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 
@@ -49,5 +48,4 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
-
 }
