6cc14685826ca40f8c1b8001465602210ead2b7b
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-01 05:36:11 -0800

    Copy strinv

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strncpy - Copies a string
+ * @dest: first pointer
+ * @src: second pointer
+ * @n: integer
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
