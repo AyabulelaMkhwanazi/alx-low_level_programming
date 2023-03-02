4193e8f25ac44cd63e89f96a11ae22f2b31f46cb
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-01 20:13:14 -0800

    Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,17 @@
  * _strcmp - Compares 2 strings
  * @s1: first string
  * @s2: second string
- * Return: integer <= 0
+ * Return: result
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int c = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (s1[c] != '\0' && s2[c] != '\0')
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		if (s1[c] != s2[c])
+			return (s1[c] - s2[c]);
 		c++;
 	}
 	return (0);
