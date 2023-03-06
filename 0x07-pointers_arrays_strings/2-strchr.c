06734ef8707bf3ae09cbdf380af0ea49b06b16a4
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-06 06:00:33 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
  * _strchr - function that locates a character in a string
  * @s: string to search
- * @c: character to search
+ * @c: character to search in s
  * Return: address (success), else NULL
  */
 
@@ -17,6 +17,7 @@ char *_strchr(char *s, char c)
 		{
 			return (&s[i]);
 		}
+		i++;
 	}
 	return (0);
 }
