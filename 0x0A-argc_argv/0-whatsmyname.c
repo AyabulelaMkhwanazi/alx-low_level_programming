aceabbd1ad1890f36a024262b4103ee62836c748
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-10 16:41:55 -0800

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @arg: argument count
+ * @argv: argument vector
+ * Description - programming prints its name followed by a new line
+ * Return: 0 (success)
+ */
+
+int main(int arg, char *argv[])
+{
+	printf("./mynameis\n");
+	return (0);
+}
