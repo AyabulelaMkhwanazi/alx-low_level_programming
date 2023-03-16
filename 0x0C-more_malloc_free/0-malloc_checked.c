728c9e386848d434073271aa65c3425086c2aa6d
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-16 16:34:52 -0700

    allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	char *s;
-
-	s = malloc(b);
-
-	if (s == NULL)
-		exit (98);
-
-	return (s);
+	void *ptr;
+		
+	ptr = malloc(b);
+		
+	if (ptr == NULL)
+		exit(98);
+			
+	return (ptr);
 }
