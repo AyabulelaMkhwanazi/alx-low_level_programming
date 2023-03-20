7f4cdf7743315657f18963a69afbcd340c0a9e67
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-20 08:32:25 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,8 +9,7 @@
 
 int _strlen(char *s)
 {
-	int i;
-	i = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
@@ -30,8 +29,8 @@ int _strlen(char *s)
 
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
-	len = 0;
+	int i;
+	int len = 0;
 
 	while (src[len] != '\0')
 	{
