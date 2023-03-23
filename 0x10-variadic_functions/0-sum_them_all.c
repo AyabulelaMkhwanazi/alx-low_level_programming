44e21ddecd3efbf66a7d0486bd7b4cd312b0f614
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-03-23 05:46:50 -0700

    Sum of all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0;
 
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		int s = va_arg(args, int);
+
 		sum += s;
 	}
 	va_end(args);
-	return(sum);
+	return (sum);
 }
