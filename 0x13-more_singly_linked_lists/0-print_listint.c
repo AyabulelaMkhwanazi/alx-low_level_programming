bcb027aa9c988601715fae0753135b70f285d430
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-04-03 16:10:59 +0200

    Traversing a singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - function prints all the elements of a listint_t list
+ * @h: pointer to the listint_t list
+ * Return: number of nodes 
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t numNodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		numNodes++;
+		h = h->next;
+	}
+	return (numNodes);
+}
