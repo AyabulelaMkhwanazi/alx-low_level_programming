63074164e941a225c36c682fe49bf58282799d66
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-04-07 14:13:45 +0200

    Binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int digit = 0;
-	unsigned int converted;
 
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-
 		digit = 2 * digit + b[i] - '0';
-		converted = digit;
+
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-	return (converted);
+	return (digit);
 }
