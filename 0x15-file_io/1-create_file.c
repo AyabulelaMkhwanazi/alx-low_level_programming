d46a560395876c9e723c4d9d3ef976dcca6ab0f5
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-04-10 12:51:48 +0200

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[s])
+		while (text_content[s] != '\0')
 			s++;
 	}
 
-	b = write(fd, text_content, sizeof(text_content));
+	b = write(fd, text_content, s);
 
 	if (fd == -1 && b == -1)
 	{
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[s])
+		while (text_content[s] != '\0')
 			s++;
 	}
 	fd = open(filename, O_WRONLY | O_APPEND, 00600);
