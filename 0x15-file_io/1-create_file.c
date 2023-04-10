174996f34032de1af47d5c0fa1ee8fa9f756bac4
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-04-10 12:45:12 +0200

    file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * create_file - function creates a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ * Return: 1 (success), else -1 (failure)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int s = 0;
+	int b;
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, (00600));
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[s])
+			s++;
+	}
+
+	b = write(fd, text_content, sizeof(text_content));
+
+	if (fd == -1 && b == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - function taht appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: string to add at the end of the file
+ * Return: 1 (success), else -1 (failure)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int r;
+	int s = 0; /* to iterate through the text_content string */
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[s])
+			s++;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND, 00600);
+	r = write(fd, text_content, s);
+
+	if (fd == -1 && r == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
