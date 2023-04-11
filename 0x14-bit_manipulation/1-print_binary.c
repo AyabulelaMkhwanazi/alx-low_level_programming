cd84f11213ddf07973fbf42131ead74219809c4c
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-04-11 03:20:10 +0200

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,10 +10,17 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, c;
+	int i;
 
-	for (i = 0; i < sizeof(int); i++)
+	for (i = 31; i >= 0; i--)
 	{
-
+		if ((n >> i) & 1)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
 	}
 }
