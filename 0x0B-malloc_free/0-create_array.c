9463f7a9cdf5778ae9e5d31d24f2da9b69b498c1
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-06-14 00:25:15 +0200

    Updated: created a function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,5 +9,23 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+	unsigned int i = 0;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc(sizeof(char) * size);
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		i++;
+		str[i] = c;
+	}
+	return (str);
 }
