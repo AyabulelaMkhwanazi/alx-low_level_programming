3da7ccc3db118a90b90b8e8b4df2186039f7fbfa
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-06-17 02:04:37 +0200

    Updated: Multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_colloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
