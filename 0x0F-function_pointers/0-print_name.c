6185fb07504b007623ec260c0a7c61e13651e6d2
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-06-22 01:59:40 +0200

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: the name.
+ * @f: function pointer
+ *
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+	f(name);
+	}
+}
