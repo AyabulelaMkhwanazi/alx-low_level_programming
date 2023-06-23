bd37a8f368eeb92392952a8831261dc0a1f55d88
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-06-23 08:02:21 +0200

    Updated: remove new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%s\n", separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
