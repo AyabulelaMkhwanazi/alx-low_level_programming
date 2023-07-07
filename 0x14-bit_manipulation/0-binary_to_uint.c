1268501a53f25aa42b1801f87583e16694305ac5
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-07-07 14:40:44 +0200

    Bit Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: pointer to the string 0 and 1 chars
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    int i;
+    unsigned int dec = 0;
+
+    if (b == NULL)
+    {
+        return (0);
+    }
+
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+        {
+            return (0);
+        }
+        dec = 2 * dec + (b[i] - '0');
+    }
+    return (dec);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the decimal
+ * Return: binary
+ */
+
+void print_binary(unsigned long int n)
+{
+    int i;
+    int count = 0;
+    unsigned long int decimal;
+
+    for (i = 63; i >= 0; i--)
+    {
+        decimal = n >> i;
+
+        if (decimal & 1)
+        {
+            _putchar('1');
+            count++;
+        }
+        else if (count)
+        {
+            _putchar('0');
+        }
+    }
+    if (!count)
+    {
+        _putchar('0');
+    }
+}
