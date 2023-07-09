5ede344118295c1c21565479070fc9129e8cb60e
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-07-09 06:13:13 +0200

    Flip Bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 	unsigned long int x = n ^ m;
 
-	while ( x != 0)
+	while (x != 0)
 	{
 		count += x & 1;
 		x >>= 1;
