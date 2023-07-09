7ccb2e5ab5ce064200a6c55b6baade0299f41e1c
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-07-09 05:59:30 +0200

    Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
-
-	if (bit_val > 63)
-	{
+	if (index >= sizeof(n) * 8)
 		return (-1);
-	}
 
-	bit_val = (n >> index) & 1;
-
-	return (bit_val);
+	return ((n >> index) & 1);
 }
