f20ddc451b84b574c4c4027ce46306ece5a9a605
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-04 11:09:49 +0200

    Task 0 completed, prints the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Program prints its name, followed by a new line
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
