61db17c2f393d8477193ebf74b2904465c0b2a90
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-04 12:31:35 +0200

    Task 2 completed: Prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,15 +4,20 @@
 /**
  * main - entry point
  *
- * Description: Program prints the number of arguments passed into it
+ * Description: Program prints all arguments it receives
  *
  * @argc: argument count (number of arguments passed into the program)
  * @argv: argument vector (array of arguments passed into the program)
  *
  * Return: always 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
 	return (0);
 }
