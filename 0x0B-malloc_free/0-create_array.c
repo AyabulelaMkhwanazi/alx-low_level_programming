15ba9a0b6abc110d3a516b7538a365149e135ac3
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-08 15:42:01 +0200

    malloc handled

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,6 +21,11 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(sizeof(char) * size);
 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
 	for (index = 0; index < size; index++)
 	{
 		array[index] = c;
