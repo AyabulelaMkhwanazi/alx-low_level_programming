2d082e48f66c978d266f657e4b8a6381dd28eafe
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-15 03:34:36 +0200

    preprocessor

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old_ptr;
 	char *new_ptr;
 	unsigned int size;
-	int i;
+	unsigned int i;
 
 	if (new_size == old_size)
 	{
