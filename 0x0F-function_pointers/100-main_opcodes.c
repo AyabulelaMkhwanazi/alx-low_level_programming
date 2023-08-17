9d02c5a2a57e4b8da6e1910e66f6f4c11330bf83
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-17 11:28:27 +0200

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints the opcodes of its own main function
+ *
+ * @argc: argument count (number of arguments passed to the
+ * program)
+ * @argv: argument vector (array of arguments passed to the
+ * program)
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *ptr;
+
+	int size;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	size = atoi(argv[1]);
+
+	if (size < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (char *)(main);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hx ", *(ptr + i) & 0xff);
+	}
+	printf("\n");
+	return (0);
+}
