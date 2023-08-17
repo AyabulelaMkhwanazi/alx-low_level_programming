84f62d35c251925e2b2047147e0fcd893bf7591c
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-17 04:15:35 +0200

    func_ptr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size <= 0)
 	{
-		
+		return (-1);
 	}
 
+	for (i = 0; i < size; i++)
+	{
+		(*cmp)(array[i]);
+	}
+
+	if (!(*cmp)(array[i]))
+	{
+		return (-1);
+	}
+	return (i);
 }
