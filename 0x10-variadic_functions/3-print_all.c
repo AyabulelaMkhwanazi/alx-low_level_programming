fd1ad20d57dc5fa794b83c778050be65adbab9dd
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-18 15:20:56 +0200

    check'd

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,7 +17,6 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 
 	va_start(all, format);
-
 	if (format != NULL)
 	{
 		while (format[i])
@@ -27,25 +26,20 @@ void print_all(const char * const format, ...)
 				case 'c':
 						printf("%s%c", separator, va_arg(all, int));
 						break;
-
 				case 'i':
 						printf("%s%d", separator, va_arg(all, int));
 						break;
-
 				case 'f':
 						printf("%s%f", separator, va_arg(all, double));
 						break;
-
 				case 's':
-						s = va_arg(all, char *);
-
-						if (s == NULL)
-						{
-							printf("(nil)");
-						}
-						printf("%s%s", separator, s);
-						break;
-
+					s = va_arg(all, char *);
+					if (s == NULL)
+					{
+						printf("(nil)");
+					}
+					printf("%s%s", separator, s);
+					break;
 				default:
 				i++;
 						continue;
