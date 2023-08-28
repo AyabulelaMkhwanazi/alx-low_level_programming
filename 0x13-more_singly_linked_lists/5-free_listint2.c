e875661021c77ae3d29653bfebaf1149fa047828
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-08-29 00:43:58 +0200

    task 5.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL)
+	listint_t *current = *head;
+	listint_t *next_node;
+	
+	if (head == NULL || *head == NULL)
 	{
 		return;
 	}
-	while (*head)
+	while (current != NULL)
 	{
-		free_listint2(&((*head)->next));
-		free(*head);
+		next_node = current->next;
+		free(current);
+		current = next_node;
 	}
-	*head = NULL;
+	*head == NULL;
 }
