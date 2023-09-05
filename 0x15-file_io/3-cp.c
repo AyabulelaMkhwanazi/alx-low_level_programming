ee4b6cf710e97225cdd12262aef29a5c5248bf72
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-09-05 09:48:36 +0200

    changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,17 +12,14 @@
 */
 int main(int argc, char *argv[])
 {
-	int file_from;
-	int file_to;
+	int file_from, file_to, nread, nwrite;
 	char *buffer[BUFFER_SIZE];
-	int nread, nwrite;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	nread = read(file_from, buffer, BUFFER_SIZE);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -39,7 +36,6 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-
 		nread = read(file_from, buffer, BUFFER_SIZE);
 		file_to = open(argv[2], O_WRONLY | O_APPEND);
 
