8d77aa725d1d50da08e7f003ba4d27f5adf039e0
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-09-05 02:47:50 +0200

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,17 +20,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		return (0);
+		return (-1);
 	}
 
 	if (text_content != NULL)
 	{
 		nwrite = write(fd, text_content, _strlen(text_content));
 	}
-	
+
 	if (nwrite == -1)
 	{
 		return (-1);
