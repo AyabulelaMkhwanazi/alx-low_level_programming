3146833d1ab4cd332ea305eb05415e344315e70e
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2023-09-29 17:37:30 +0200

    get bit bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,9 +14,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
-	if (n == 0 && index == 0)
+	if (n == 0)
 	{
 		return (0);
+	}
+	if (index > 31)
+	{
+		return (-1);
 	}
 	else
 	{
