dbbbee54d1f16fa2ce08bab353db4b70d888523f
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-01-18 16:41:07 +0200

    8. Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	}
 
-	while (*head != NULL && counter < index)
+	while (tmp != NULL && counter < index)
 	{
 		tmp = tmp->next;
 		counter++;
